e3cc51c4d9d28749c1e831d2552cfaa314408dd2
Richard <richokoye1@gmail.com>
2020-06-11 02:28:57 +0100

    Updates

---
+++ precache-manifest.ef87b33d8dd88f4b2f9c96f75338a23b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3ab3c06cb77672df01ddf6b1520f40",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "1a3ae43c4cc1802080f3",
+    "url": "/react-movies/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "acb7bcaf433bf490b4a9",
+    "url": "/react-movies/static/css/main.1ac5bd92.chunk.css"
+  },
+  {
+    "revision": "1a3ae43c4cc1802080f3",
+    "url": "/react-movies/static/js/2.305264c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies/static/js/2.305264c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb7bcaf433bf490b4a9",
+    "url": "/react-movies/static/js/main.2bf6f889.chunk.js"
+  },
+  {
+    "revision": "e1de1a3a52a93e74a8db",
+    "url": "/react-movies/static/js/runtime-main.01d168e1.js"
+  }
+]);
